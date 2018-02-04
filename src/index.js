1bcac7400551cb1af2b23528a9f288dc17310b6c
alberto hortelano <alberto.hortelano.moran@gmail.com>
2018-02-04 16:59:26 +0000

    refactor RomanNumerals as class

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 'use strict';
 
-function RomanNumerals(numeral){
+class RomanNumerals {
+	constructor (numeral) {
+		const romanSimbols = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
+		// Sanitize input
+		this.sanitize (numeral);
+	}
+
+	sanitize (numeral) {
+		if (numeral === null || numeral === undefined || numeral === '' ) {
+			throw new Error('value required');
+		} else if (Number.isInteger(numeral) &&
+			(numeral < 1 || numeral > 3999)
+		) {
+			throw new Error('invalid range');
+		} else {
+			this.input = numeral;
+		}
+
+	}
 }
 const romanNumeral = (numeral) => {
-	if (numeral === null || numeral === undefined || numeral === '' ) {
-		throw new Error('value required');
-	} else if (Number.isInteger(numeral) &&
-		(numeral < 1 || numeral > 3999)
-	) {
-		throw new Error('invalid range');
-	}
 	return new RomanNumerals(numeral);
 }
 
