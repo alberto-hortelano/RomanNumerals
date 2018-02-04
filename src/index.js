53e3b3d96264ec73b13a84fd3414dfe7ee668421
alberto hortelano <alberto.hortelano.moran@gmail.com>
2018-02-04 17:10:08 +0000

    start method to check if is valid roman number

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
 'use strict';
 
+const romanSimbols = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
+
 class RomanNumerals {
 	constructor (numeral) {
-		const romanSimbols = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
 		// Sanitize input
 		this.sanitize (numeral);
 	}
 
 	sanitize (numeral) {
+		// Empty input
 		if (numeral === null || numeral === undefined || numeral === '' ) {
 			throw new Error('value required');
-		} else if (Number.isInteger(numeral) &&
+		// Out of range
+		} else if (
+			Number.isInteger(numeral) &&
 			(numeral < 1 || numeral > 3999)
 		) {
 			throw new Error('invalid range');
+		// Invalid value
+		} else if (
+			!Number.isInteger(numeral) ||
+			(
+				(typeof numeral === 'string' || numeral instanceof String) &&
+				this.isValidRoman(numeral)
+			)
+		) {
+
 		} else {
 			this.input = numeral;
 		}
 
+	}
+	isValidRoman (numeral) {
+		return true;
 	}
 }
 const romanNumeral = (numeral) => {
